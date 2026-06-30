5d660024089c55101302e36ee13fab69b4398b2f
manucourtade <courtademanuel@outlook.es>
2026-06-30 16:27:47 -0300

    first commit

---
+++ src/test/java/com/kiosccourtade/kiosc_api/KioscApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiosccourtade.kiosc_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KioscApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
