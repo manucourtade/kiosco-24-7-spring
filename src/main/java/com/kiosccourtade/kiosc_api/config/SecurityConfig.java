22b0bcf62dc2e37d8576760ed8397f862f7b5dd4
manucourtade <courtademanuel@outlook.es>
2026-07-07 18:44:28 -0300

    add swagger public

--- src/main/java/com/kiosccourtade/kiosc_api/config/SecurityConfig.java
+++ src/main/java/com/kiosccourtade/kiosc_api/config/SecurityConfig.java
@@ -23,6 +23,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session
